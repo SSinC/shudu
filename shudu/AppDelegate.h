4be3d906927d2c0845002c92b1e95d68af36360f
wktzjz <wktzjz@gmail.com>
2014-06-25 14:20:28 +0800

    Initial Commit

---
+++ shudu/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  shudu
+//
+//  Created by Stan on 14-6-25.
+//  Copyright (c) 2014年 Stan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ shudu/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  shudu
+//
+//  Created by Stan on 14-6-25.
+//  Copyright (c) 2014年 Stan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
